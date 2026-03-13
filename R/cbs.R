37ac12a3ae7ea0ee1611690e5e7f4347292ac7e0
Matthijs Berends <m.s.berends@umcg.nl>
2026-03-13 10:53:23 +0100

    add `amrznn_upload()`

--- R/cbs.R
+++ R/cbs.R
@@ -15,7 +15,7 @@
 #  We created this package for both routine data analysis and academic  #
 #  research and it was publicly released in the hope that it will be    #
 #  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
-# ===========================================================
+# ===================================================================== #
 
 #' Download CBS-data
 #'
